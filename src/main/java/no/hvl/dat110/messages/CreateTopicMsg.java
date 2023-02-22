99502f22a488b7f6cfa2ad3a633fcd8a503e0fcd
krida <krida@LAPTOP-2475AL2U>
2023-02-22 23:23:13 +0100

    Begynt på CreateTopicMsg

--- src/main/java/no/hvl/dat110/messages/CreateTopicMsg.java
+++ src/main/java/no/hvl/dat110/messages/CreateTopicMsg.java
@@ -1,16 +1,34 @@
 package no.hvl.dat110.messages;
 
+// TODO: 
+// Implement object variables - a topic is required
+
+// Complete the constructor, get/set-methods, and toString method
+// as described in the project text	
+
 public class CreateTopicMsg extends Message {
 	
 	// message sent from client to create topic on the broker
+	
+	private String topic;
 
     public CreateTopicMsg(String user, String topic) {
-
+    	super(); //user??
+    	this.topic = topic;
     }
 
-	// TODO: 
-	// Implement object variables - a topic is required
-	
-	// Complete the constructor, get/set-methods, and toString method
-    // as described in the project text	
+	public String getTopic() {
+		return topic;
+	}
+
+	public void setTopic(String topic) {
+		this.topic = topic;
+	}
+
+	@Override
+	public String toString() {
+		return "CreateTopicMsg [topic=" + topic + "]"; //autogenerert
+	}
+    
+
 }
