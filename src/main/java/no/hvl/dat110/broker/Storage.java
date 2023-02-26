de75a3c3b9061944321767c64438ec3a8abeb899
krida <krida@LAPTOP-2475AL2U>
2023-02-26 20:48:39 +0100

    Ferdig med Storage-klassen

--- src/main/java/no/hvl/dat110/broker/Storage.java
+++ src/main/java/no/hvl/dat110/broker/Storage.java
@@ -16,7 +16,6 @@ public class Storage {
 	
 	// data structure for managing currently connected clients
 	// maps from user to corresponding client session object
-	
 	protected ConcurrentHashMap<String, ClientSession> clients;
 
 	public Storage() {
@@ -36,7 +35,6 @@ public class Storage {
 
 	// get the session object for a given user
 	// session object can be used to send a message to the user
-	
 	public ClientSession getSession(String user) {
 
 		ClientSession session = clients.get(user);
@@ -50,52 +48,46 @@ public class Storage {
 
 	}
 
+	// add corresponding client session to the storage
 	public void addClientSession(String user, Connection connection) {
-
-		// TODO: add corresponding client session to the storage
-		// See ClientSession class
 		
-		throw new UnsupportedOperationException(TODO.method());
+		ClientSession session = new ClientSession(user, connection);
+		clients.put(user, session);
 		
 	}
 
+	// disconnect the client (user) and remove client session for user from the storage
 	public void removeClientSession(String user) {
 
-		// TODO: disconnet the client (user) 
-		// and remove client session for user from the storage
-		
-		throw new UnsupportedOperationException(TODO.method());
+		clients.remove(user);
 		
 	}
 
+	// create topic in the storage
 	public void createTopic(String topic) {
 
-		// TODO: create topic in the storage
-
-		throw new UnsupportedOperationException(TODO.method());
+		Set<String> subscribers = ConcurrentHashMap.newKeySet();
+		subscriptions.put(topic, subscribers);
 	
 	}
-
+	
+	// delete topic from the storage
 	public void deleteTopic(String topic) {
 
-		// TODO: delete topic from the storage
-
-		throw new UnsupportedOperationException(TODO.method());
+		subscriptions.remove(topic);
 		
 	}
 
+	// add the user as subscriber to the topic
 	public void addSubscriber(String user, String topic) {
-
-		// TODO: add the user as subscriber to the topic
 		
-		throw new UnsupportedOperationException(TODO.method());
+		subscriptions.get(topic).add(user);
 		
 	}
 
+	// remove the user as subscriber to the topic
 	public void removeSubscriber(String user, String topic) {
 
-		// TODO: remove the user as subscriber to the topic
-
-		throw new UnsupportedOperationException(TODO.method());
+		subscriptions.get(topic).remove(user);
 	}
 }
